1d8884f4a21da305a9f4db0b69eb230ef4554328
Victor Castro <victor@castrocontreras.com>
2018-02-13 10:50:44 -0500

    Update .bash_mealiases.sh

--- .bash_mealiases.sh
+++ .bash_mealiases.sh
@@ -1,4 +1,9 @@
-# some more ls aliases
+# terminal
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
+
+# git
+lg1 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
+lg2 = log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all
+lg = !"git lg1"
